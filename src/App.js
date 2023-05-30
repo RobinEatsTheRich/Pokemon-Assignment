08d70b4391015955f99b103433d1f1f397e5d41f
RobinEatsTheRich <rvasselt99@gmail.com>
2023-05-30 16:01:46 +0200

    initialising assignment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+    async function fetchData() {
+        try {
+            const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/ditto`);
+            console.log(result.data);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+
   return (
     <div>
       Begin hier met de opdracht!
